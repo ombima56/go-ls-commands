3865234098f27f7b7e7c5653b1c636b8ff2adffe
ombima56 <ombimahillary6@gmail.com>
2024-10-16 05:07:13 +0300

    feat: implement ListFile function only to list files based on wether -l or -a flag is passed

--- listfiles/listFiles.go
+++ listfiles/listFiles.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/user"
 	"strconv"
+	"strings"
 	"syscall"
 	"time"
 )
@@ -17,67 +18,87 @@ type FileInfo struct {
 
 func FileModeToString(mode os.FileMode) string {
 	var perm string
-    if mode.IsDir() {
-        perm = "d"
-    } else {
-        perm = "-"
-    }
+	if mode.IsDir() {
+		perm = "d"
+	} else {
+		perm = "-"
+	}
 
-    perms := []rune(mode.String())
-    for _, r := range perms[1:10] {
-        perm += string(r)
-    }
+	perms := []rune(mode.String())
+	for _, r := range perms[1:10] {
+		perm += string(r)
+	}
 
-    return perm
+	return perm
 }
 
 func PrintFileInfo(file os.FileInfo) {
 	stat := file.Sys().(*syscall.Stat_t)
-    owner, _ := user.LookupId(strconv.Itoa(int(stat.Uid)))
-    group, _ := user.LookupGroupId(strconv.Itoa(int(stat.Gid)))
+	owner, _ := user.LookupId(strconv.Itoa(int(stat.Uid)))
+	group, _ := user.LookupGroupId(strconv.Itoa(int(stat.Gid)))
 
-    // Get file mode (permissions), number of links, owner, group, size
-    permissions := FileModeToString(file.Mode())
-    numLinks := stat.Nlink
-    fileSize := file.Size()
+	// Get file mode (permissions), number of links, owner, group, size
+	permissions := FileModeToString(file.Mode())
+	numLinks := stat.Nlink
+	fileSize := file.Size()
 
-    // Format modification time
-    modTime := file.ModTime().Format("Jan 02 15:04")
+	// Format modification time
+	modTime := file.ModTime().Format("Jan 02 15:04")
 
-    // Print information in ls -l format
-    fmt.Printf("%s %d %s %s %6d %s %s\n",
-        permissions,
-        numLinks,
-        owner.Username,
-        group.Name,
-        fileSize,
-        modTime,
-        file.Name(),
-    )
+	// Print information in ls -l format
+	fmt.Printf("%s %d %s %s %6d %s %s\n",
+		permissions,
+		numLinks,
+		owner.Username,
+		group.Name,
+		fileSize,
+		modTime,
+		file.Name(),
+	)
 }
 
 func printFileName(file os.FileInfo) {
 	fmt.Printf("%s ", file.Name())
 }
 
-func ListFiles(path string) {
+func ListFiles(path string, longFormat bool, allFiles bool) {
 	files, err := os.ReadDir(path)
 	if err != nil {
 		fmt.Println("Error: ", err)
 		return
 	}
 
-	var totalSize int64
+	if longFormat {
+		var totalSize int64
+		for _, file := range files {
+			fileInfo, _ := file.Info()
 
-	for _, file := range files {
-		fileInfo, _ := file.Info()
-		totalSize += fileInfo.Size()
+			// Skip hidden files if the -a flag is not set
+			if !allFiles && strings.HasPrefix(fileInfo.Name(), ".") {
+				continue
+			}
+
+			totalSize += fileInfo.Size()
+		}
+		fmt.Printf("total %d\n", totalSize/1024)
 	}
 
-	fmt.Printf("total %d\n", totalSize/1024) // Display total size in kilobytes
-
 	for _, file := range files {
 		fileInfo, _ := file.Info()
-		PrintFileInfo(fileInfo)
+
+		// Skip hidden files if the -a flag is not set
+		if !allFiles && strings.HasPrefix(fileInfo.Name(), ".") {
+			continue
+		}
+
+		if longFormat {
+			PrintFileInfo(fileInfo)
+		} else {
+			printFileName(fileInfo)
+		}
+	}
+
+	if !longFormat {
+		fmt.Println()
 	}
 }
