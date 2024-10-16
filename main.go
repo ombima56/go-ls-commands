0465533649ce4475ae419f09416ea21c4b34fea3
ombima56 <ombimahillary6@gmail.com>
2024-10-16 06:39:47 +0300

    feat: refactor main.go

--- main.go
+++ main.go
@@ -8,32 +8,19 @@ import (
 )
 
 func main() {
-	dir := "."
-	showDetails := false
-	includeHidden := false
-	reverseOrder := false
-	sortByTime := false
-	recursive := false
+	args := os.Args[1:]
+	path := "."
 
-	for i := 1; i < len(os.Args); i++ {
-		args := os.Args[i]
-		switch args {
-		case "-l":
-			showDetails = true
-		case "-R":
-			recursive = true
-		case "-a":
-			includeHidden = true
-		case "-r":
-			reverseOrder = true
-		case "-t":
-			sortByTime = true
-		default:
-			dir = args
-		}
+	if len(args) == 0 {
+		listfiles.ListFiles(".", false, false)
+		return
 	}
 
-	if err := listfiles.ListFiles(dir, showDetails, recursive, includeHidden, reverseOrder, sortByTime); err != nil {
-		fmt.Println("Error:", err)
+	longFlag, allFlag, err := listfiles.ValidateFlags(args)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
 	}
+
+	listfiles.ListFiles(path, longFlag, allFlag)
 }
