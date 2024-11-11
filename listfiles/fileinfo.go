04b199595f9827dd3a95a3ac51b85a319a12a9c7
ombima56 <ombimahillary6@gmail.com>
2024-11-11 22:56:37 +0300

    feat: add smbolick functionalit to check fot the links

--- listfiles/fileinfo.go
+++ listfiles/fileinfo.go
@@ -10,7 +10,7 @@ import (
 	"go-ls-commands/colors"
 )
 
-func PrintFileInfo(file os.FileInfo) {
+func PrintFileInfo(path string, file os.FileInfo) {
 	stat := file.Sys().(*syscall.Stat_t)
 	owner, _ := user.LookupId(strconv.Itoa(int(stat.Uid)))
 	group, _ := user.LookupGroupId(strconv.Itoa(int(stat.Gid)))
@@ -19,15 +19,25 @@ func PrintFileInfo(file os.FileInfo) {
 	permissions := FileModeToString(file.Mode())
 	numLinks := stat.Nlink
 	fileSize := file.Size()
-
-	// Format modification time
 	modTime := file.ModTime().Format("Jan _2 15:04")
-
-	// Determine the output color based on whether it is a directory
 	color := colors.GetFileColor(file)
 
+	// Prepare symlink info if the file is a symlink
+	symlinkInfo := ""
+	if file.Mode()&os.ModeSymlink != 0 {
+		fullPath := path + "/" + file.Name() // Manually construct the full path
+		target, err := os.Readlink(fullPath)
+		if err == nil {
+			symlinkInfo = fmt.Sprintf(" -> %s", target)
+		} else {
+			symlinkInfo = " -> [broken link]"
+		}
+		// Adjust permissions display to indicate it's a symlink
+		permissions = "l" + permissions[1:]
+	}
+
 	// Print information in ls -l format
-	fmt.Printf("%s %d %s %s %4d %s %s%s%s\n",
+	fmt.Printf("%s %d %s %s %4d %s %s%s%s%s\n",
 		permissions,
 		numLinks,
 		owner.Username,
@@ -36,6 +46,7 @@ func PrintFileInfo(file os.FileInfo) {
 		modTime,
 		color,
 		file.Name(),
+		symlinkInfo,
 		colors.Reset,
 	)
 }
